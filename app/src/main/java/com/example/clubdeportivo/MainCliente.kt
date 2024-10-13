f1c83eef727c877e40b67e358f849d948441ec1f
Heber Duarte <Heberduarteryr@gmail.com>
2024-10-13 19:03:10 -0300

    Primeros cambios

--- app/src/main/java/com/example/clubdeportivo/MainCliente.kt
+++ app/src/main/java/com/example/clubdeportivo/MainCliente.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,11 +17,17 @@ class MainCliente : AppCompatActivity() {
         setContentView(R.layout.registro_cliente)
         val button = findViewById<Button>(R.id.btn_nav_home)
         val btnRegistro = findViewById<Button>(R.id.btnClienteRegistro)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupSocio)
         button.setOnClickListener{
             Utils.cambioPantalla(this,MainMenu::class.java)
         }
         btnRegistro.setOnClickListener{
+            if(radioGroup.checkedRadioButtonId == R.id.radioButtonSocio){
             Utils.cambioPantalla(this, MainPagoCuotaSocio::class.java)
+            } else {
+            Utils.cambioPantalla(this, MainPagoCuotaNoSocio::class.java)
+            }
+
         }
 
     }
