c7f7bfbd124492df8f5770a6d3dcb6f2827545b1
Heber Duarte <Heberduarteryr@gmail.com>
2024-10-23 03:15:34 -0300

    Modal definido

--- app/src/main/java/com/example/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.clubdeportivo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),  ModalFragment.ModalListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,14 +19,29 @@ class MainActivity : AppCompatActivity() {
         //aqui hace el cambio de pantalla
 
         val button = findViewById<Button>(R.id.btnLogin)
+        var pass = true
+
         button.setOnClickListener {
-            Utils.cambioPantalla(this, MainMenu::class.java)
+            if (pass) {
+                pass = false
+                val modal = ModalFragment.newInstance("Campos  invalidos", "No se aceptan campos vacios", "OK", "Reiniciar")
+                modal.show(supportFragmentManager, "ModalFragment")
+            } else {
+                Utils.cambioPantalla(this, MainMenu::class.java)
+            }
+
         }
 
         val buttonRegister = findViewById<TextView>(R.id.txtResgitrate)
         buttonRegister.setOnClickListener {
             Utils.cambioPantalla(this, MainUsuario::class.java)
         }
+    }
+
+    override fun onModalResult(success: Boolean) {
+        // Esta funcion se ejecuta al cerrar el modal
+        var email = findViewById<TextView>(R.id.editLoginEmail)
+        email.text =  if(success) "success" else "reject"
     }
 
 
